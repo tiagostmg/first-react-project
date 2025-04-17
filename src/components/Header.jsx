bda874b59f4007e02b0f6533891cba84675294ba
tiagostmg <tiago.taumaturgo@gmail.com>
2025-04-17 00:16:50 -0300

    refactor: update Header component to use logo image and adjust styles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import logo from '../assets/react.svg'
+
 export function Header() {
     return (
       <header className={styles.header}>
-        <strong>Ignite Feed</strong>
+        <img src={logo} alt="React logo" />
       </header>
     )
 }
